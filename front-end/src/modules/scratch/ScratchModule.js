9467c1e4ac5f8f96c78b31a6addb2bae9954d233
Bernice Wong <bew225@lehigh.edu>
2024-04-02 15:19:31 -0400

    Scratch module TOC

--- front-end/src/modules/scratch/ScratchModule.js
+++ front-end/src/modules/scratch/ScratchModule.js
@@ -1,30 +1,56 @@
-import React from 'react';
+// import React from 'react';
+import React, { useEffect } from 'react';
 import './ScratchModule.css';
 
 import scratchImg from './Scratch.png';
 import scratchBlock from './ScratchBlocks.png'
 
 function SkeletonModule() {
+    useEffect(() => {
+        // Add event listeners to all TOC links
+        const tocLinks = document.querySelectorAll('.toc a');
+        tocLinks.forEach(link => {
+            link.addEventListener('click', scrollToSection);
+        });
+
+        // Remove event listeners when component unmounts
+        return () => {
+            tocLinks.forEach(link => {
+                link.removeEventListener('click', scrollToSection);
+            });
+        };
+    }, []);
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const targetId = event.target.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            const yOffset = -80; // Adjust as needed to consider any fixed header
+            const rect = targetSection.getBoundingClientRect();
+            const scrollPosition = rect.top + window.scrollY + yOffset;
+            window.scrollBy({ top: scrollPosition, left: 0, behavior: 'smooth' });
+        }
+    }
     return (
         <div class="scratch">
-            <h1>Introduction to Scratch:<br></br><i>Creative Coding</i></h1>
-            <img src={scratchImg} alt="Scratch"></img>
-            {/* <div class="left-column">
+            { <div class="toc">
                 <h2>Table of Contents</h2>
                 <ul>
-                    <li>STEELS Standards</li>
-                    <li>Objectives</li>
-                    <li>Materials</li>
-                    <li>Basic Vocab</li>
-                    <li>Introduction</li>
-                    <li>Ceaser Cipher</li>
-                    <li>Class Activity</li>
-                    <li>Summary</li>
-                    <li>Discussion</li>
+                    <li><a href="#STEELS Standards">STEELS Standards</a></li>
+                    <li><a href="#Objectives">Objectives</a></li>
+                    <li><a href="#Materials">Materials</a></li>
+                    <li><a href="#Basic Vocab">Basic Vocab</a></li>
+                    <li><a href="#Introduction">Introduction</a></li>
+                    <li><a href="#Class Activity">Class Activity</a></li>
+                    <li><a href="#Summary">Summary</a></li>
+                    <li><a href="#Discussion">Discussion</a></li>
                 </ul>
-            </div> */}
+            </div> }
+            <h1>Introduction to Scratch:<br></br><i>Creative Coding</i></h1>
+            <img src={scratchImg} alt="Scratch"></img>
             <div class="body">
-                <h2>STEELS Standards</h2>
+                <h2 id="STEELS Standards">STEELS Standards</h2>
                     <ul>
                         <li><a href="https://files5.pdesas.org/250014226114139172173254247149098013233116174016/Download.ashx?hash=2.2">3.5.6-8.G</a></li>
                         <li><a href="https://files5.pdesas.org/055216065045142101091198140253174218018106005019/Download.ashx?hash=2.2">3.5.6-8.K</a></li>
@@ -36,18 +62,18 @@ function SkeletonModule() {
                         <li><a href="https://files5.pdesas.org/191077107162061033191092180254094011093001251131/Download.ashx?hash=2.2">3.5.6-8.X</a></li>
                         <li><a href="https://files5.pdesas.org/067057030146123187047085208090089082001012155008/Download.ashx?hash=2.2">3.5.6-8.GG</a></li>
                     </ul>
-                <h2>Objectives</h2>
+                <h2 id="Objectives">Obectives</h2>
                     <ul>
                         <li>Students will understand how to navigate the Scratch interface</li>
                         <li>Students will understand how to chain motion blocks together to create a simple animation</li>
                         <li>Students will understand basic control structures, conditionals, and loops</li>
                     </ul>
-                <h2>Materials</h2>
+                <h2 id="Materials">Materials</h2>
                     <ul>
                         <li><a href="https://scratch.mit.edu/">Scratch Website</a></li>
                         <li>A personal computer</li>
                     </ul>
-                <h2>Basic Vocab</h2>
+                <h2 id="Basic Vocab">Basic Vocab</h2>
                     <ul>
                         <li><b>Sprite</b>
                             <ul><li>The objects on the Scratch stage that perform actions</li></ul>
@@ -62,7 +88,7 @@ function SkeletonModule() {
                             <ul><li>Expressions that evaluate to either true or false and allow computers to make decisions based on a specific criteria</li></ul>
                         </li>
                     </ul>
-                <h2>Introduction</h2>
+                <h2 id="Introduction">Introduction</h2>
                     <p>Introduce Scratch as a means to create interactive stories, games, and animations. It was first launched in 2007 by the Lifelong Kindergarten Group at the MIT Media Lab and was developed with the intention of providing an intuitive platform allowing for creative expression. As one of the first kid-friendly open source resources of its time, it drew in users, teachers and students alike, and fostered a community of young minds seeking to create, with some going as far as creating extensions that improve user experience. </p>
                     <p>Although lacking the finesse of professional cartoons and animations, Scratch covers a variety of actions, environments, and stories, only limited by creativity. To demonstrate the potential of Scratch, you can display some finished projects located <a href="https://scratch.mit.edu/explore/projects/all/"><u>here</u>.</a></p>
                     <p>To navigate to your own project, click create, located near the top next to the Scratch logo</p>
@@ -85,7 +111,7 @@ function SkeletonModule() {
                 <h2>Fun Bonuses</h2>
                     <li>Access backdrop on the bottom right to change the scene</li>
                     <li>Choose a sprite on the bottom right to choose from a wide range of sprites and customize its size and direction</li>
-                <h2>Class Activity</h2>
+                <h2 id="Class Activity">Class Activity</h2>
                     <p><b>Option 1:</b></p>
                     <p>Give the class some time to independently explore Scratch and create their own animation. Perhaps help brainstorm ideas related to concepts in their other lessons or ones that teach helpful lessons. For example, an environmental awareness one based in the arctic featuring a polar bear!</p>
                     <p><b>Option 2:</b></p>
@@ -99,12 +125,12 @@ function SkeletonModule() {
                             <li>Create a virtual pet that changes outfits, sounds, and position based on mood, user input, etc.</li>
                         </ul>
                     </blockquote>
-                <h2>Summary</h2>
+                <h2 id="Summary">Summary</h2>
                     <p>Bring the class back together and have them share their animations(as applicable based on classroom activity). Discuss the strategies they employed and the challenges they faced during the activity.</p>
                     <p>Discuss how concepts (problem solving, sequencing, loops, conditionals, and event handling) introduced in Scratch mirror many engineering and coding principles. Encourage students to think about where else in their lives they may have been unconsciously utilizing these concepts. What other industries do they think these concepts are beneficial? </p>
                     <p>Emphasize the importance of creativity in Computer Science/Engineering. Coding requires more flexibility than one might think and knowing how to adapt and attack a problem from different angles will make you a successful programmer. In addition, much like with your animation, your computer science projects are only capped by your imagination!</p>
                     <p>In summary, Scratch acts as an excellent stepping stone into the world of engineering, primarily computer science. It introduces fundamental programming concepts in a visual environment and these concepts act as building blocks to understand more advanced programming languages and techniques! In addition, Scratch promotes creativity and experimentation which allows young programmers to build their problem solving skills.</p>
-                <h2>Discussion</h2>
+                <h2 id="Discussion">Discussion</h2>
                     <p><i>(Try to guide student discussion to touch on these)</i></p>
                     <ul>
                         <li>Computer Science concepts are all around us. At its core, computer science breaks down large tasks into small manageable pieces, something we already naturally do.
