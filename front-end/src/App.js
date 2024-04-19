a47f3821cba2031f62220386fe22df39ff9712d6
ktl325 <ktl325@lehigh.edu>
2024-04-18 21:04:29 -0400

    deleted client

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,11 +15,9 @@ import ScratchModule from './modules/scratch/ScratchModule';
 import MazeModule from './modules/maze/MazeModule';
 
 import { Amplify } from 'aws-amplify';
-import { generateClient } from 'aws-amplify/api';
 import config from './amplifyconfiguration.json';
 Amplify.configure(config);
 
-const client = generateClient();
 
 function App() {
   return (
